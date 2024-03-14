cdf07ed2d222d03a807dede64149f80c2cf67119
LinkWHorter <LinkWHorter@gmail.com>
2024-03-14 14:16:28 +0200

    Creating Imports for Project;

--- app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
@@ -8,9 +8,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import java.util.*
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.cw_4.ui.theme.IPZ_CW_4_Kovalov_VladislavTheme
+import ua.edu.lntu.cw_4.model.Task
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/ua/edu/lntu/cw_4/model/TaskClass.kt
@@ -0,0 +1,5 @@
+package ua.edu.lntu.cw_4.model
+
+// Модель для завдання
+data class Task(val id: Int, val title: String, val description: String, val date: String, var status: String)
+
